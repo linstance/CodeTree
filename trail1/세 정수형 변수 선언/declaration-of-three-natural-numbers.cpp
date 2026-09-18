640d1d56adf7ab5a48857fb64854f64b85d3fe1a
Jaewon_LEE <66247624+linstance@users.noreply.github.com>
2026-09-18 21:25:34 +0900

    [Passed] 세 정수형 변수 선언 (1ms, 0MB)

---
+++ trail1/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() 
+{
+    // Please write your code here.
+    int a = 7;
+    int b = 23;
+    int c = 30;
+
+    std::cout << a << " + " << b << " = " << c << "\n";
+    return 0;
+}
