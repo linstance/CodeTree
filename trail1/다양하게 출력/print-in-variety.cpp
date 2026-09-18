edd80f222a23182ca1dbaaa673afe87a369956dd
Jaewon_LEE <66247624+linstance@users.noreply.github.com>
2026-09-18 20:42:06 +0900

    [Passed] 다양하게 출력 (2ms, 0MB)

---
+++ trail1/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main() 
+{
+    std::cout << "Total days in Year\n";
+    std::cout << "365\n";
+    std::cout << "Circumference rate\n";
+    std::cout << "3.1415926535";
+    return 0;
+}
